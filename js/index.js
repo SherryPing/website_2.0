7b2040be3848eee83a1f02eb8fc80da84399ea7f
SherryPing <2998554420@qq.com>
2018-05-16 15:32:04 +0800

    页面添加

--- js/index.js
+++ js/index.js
@@ -3,11 +3,18 @@ $(function(){
 	window.onscroll = function(){
 	    var sTop = $(window).scrollTop();
 	    if(sTop>=410){
-	    	$(".z-header").css("background","#092852");
+	    	$(".z-header").css("background","#fff");
+	    	$(".z-header_content .menu>li>a").css("color","#333");
+	    	$(".z-header_content .menu>li>span").css("color","#333");
+	    	$(".z-header").addClass("bg-blue");
 	    }else{
 	    	$(".z-header").css("background","transparent");
+	    	$(".z-header_content .menu>li>a").css("color","#fff");
+	    	$(".z-header_content .menu>li>span").css("color","#fff");
+	    	$(".z-header").removeClass("bg-blue").addClass("bg-white");
+	    	$(".z-header").addClass("bg-white");
 	    }
-	    console.log(sTop)
+
 	}
 	$('.menu>li').hover(function(e) {
 	        $(this).find("ul").stop().slideToggle()
